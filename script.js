e17364dd67e649d861baaccd402a3360c6b8f364
uxdibyajeet <ux.dibyajeet@gmail.com>
2026-03-07 18:07:39 +0000

    need to flesh out i/0 logic and evaluation logic

--- script.js
+++ script.js
@@ -4,14 +4,14 @@
 const calcKeyboard = document.querySelector('.calcKeyboard');
 
 // populating the keyboard
-const inputs = ['del','ac', '%', '/',
+const inputItems = ['ac','del', '%', '/',
                 7, 8, 9, '*', 
                 4, 5, 6, '+', 
                 1, 2, 3, '-', 
                 0, '.', '=',
             ];
 const operators = ['+', '-', '.', '/', '*', '=', 'del', 'ac', '%'];
-inputs.forEach((item) => {
+inputItems.forEach((item) => {
     const newButton = document.createElement('button')
     newButton.textContent = item;
 
@@ -24,6 +24,7 @@ inputs.forEach((item) => {
     } else if (operators.includes(item)) {
         newButton.style.backgroundColor = "hsla(210, 10%, 30%, 1)";
         newButton.style.color = "#fff";
+        newButton.style.textTransform = 'uppercase'
     } else {
         newButton.style.backgroundColor = 'hsla(210, 10%, 20%, 1)';
         newButton.style.color = '#fff';
@@ -45,9 +46,48 @@ const displayText = document.createElement('p');
 calcDisplay.appendChild(displayText);
 displayText.textContent = 0;
 
-// Handle key press of user
+// Handle key press of user and realtime display of inputs
+let input = '';
+let userOperator = '';
 function handleKeyPress(value) {
-    console.log(value, typeof(value));
-    let calcDisplayContent = value;
-    displayText.textContent = calcDisplayContent;
-}
+    if (value === 'ac') {
+        input = '';
+        displayText.textContent = 0;
+    } else if (value === 'del'){
+        if (input.length >= 2){
+            input = input.slice(0, -1);
+            displayText.textContent = input;
+            // console.log(input, input.length);
+        } else {
+            input = '';
+            displayText.textContent = 0;
+            // console.log(input, input.length);
+        }
+    } else if (operators.includes(value)){
+        if (value === '=') {
+            console.log("answer")
+        } else if (value === '.'){
+            //if input number has '.' users cannot enter more '.'
+            if (!input.includes('.')){
+                userOperator = value;
+                input += userOperator;
+                console.log(input, typeof(input), userOperator);
+                displayText.textContent = input;
+            } 
+        } 
+        else {
+            // here I need to slice input into two numbers and store that in array to do math;
+            userOperator = value;
+            input = input.slice(0, -1);
+            input += userOperator;
+            console.log(input, typeof(input), userOperator);
+            displayText.textContent = input;
+        }
+    } else {
+        input += value;
+        console.log(input, typeof(input));
+        displayText.textContent = input;
+    }
+};
+
+// Handle Evaluation logic
