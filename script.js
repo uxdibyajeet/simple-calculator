4681d3be58fc9287a3befd6cafd849050bff6926
uxdibyajeet <ux.dibyajeet@gmail.com>
2026-03-07 11:27:34 +0000

    UI finalization

--- script.js
+++ script.js
@@ -4,28 +4,35 @@
 const calcKeyboard = document.querySelector('.calcKeyboard');
 
 // populating the keyboard
-const inputs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+', '-', '.', '/', '*', '=',];
-const operators = ['+', '-', '.', '/', '*', '=',];
+const inputs = ['del','ac', '%', '/',
+                7, 8, 9, '*', 
+                4, 5, 6, '+', 
+                1, 2, 3, '-', 
+                0, '.', '=',
+            ];
+const operators = ['+', '-', '.', '/', '*', '=', 'del', 'ac', '%'];
 inputs.forEach((item) => {
     const newButton = document.createElement('button')
     newButton.textContent = item;
-    // default styling
-    // newButton.style.minHeight = "2.5rem";
-    newButton.style.aspectRatio = "1/1";
-    newButton.style.fontSize = "calc(0.5rem + 1vw)";
-    newButton.style.alignItems = "center";
 
     // custom styling
     if (item === "=") {
-        newButton.style.backgroundColor = "hsla(50, 70%, 70%, 1)"
+        newButton.style.backgroundColor = "hsla(210, 10%, 95%, 1)"
         newButton.style.color = "#080808";
+        newButton.style.gridColumn = 'span 2';
+        newButton.style.aspectRatio = 'auto';
     } else if (operators.includes(item)) {
-        newButton.style.backgroundColor = "hsla(210, 70%, 30%, 1)";
+        newButton.style.backgroundColor = "hsla(210, 10%, 30%, 1)";
         newButton.style.color = "#fff";
     } else {
-        newButton.style.backgroundColor = 'hsla(210, 10%, 15%, 1)';
+        newButton.style.backgroundColor = 'hsla(210, 10%, 20%, 1)';
         newButton.style.color = '#fff';
     }
+
+    // Event Listner
+    newButton.addEventListener('click', () => {
+        handleKeyPress(item);
+    });
 
     calcKeyboard.appendChild(newButton);
 });
@@ -34,5 +41,13 @@ inputs.forEach((item) => {
 
 // code for display UI
 const calcDisplay = document.querySelector('.calcDisplay');
-let calcDisplayContent = 'text';
-calcDisplay.textContent = calcDisplayContent;
+const displayText = document.createElement('p');
+calcDisplay.appendChild(displayText);
+displayText.textContent = 0;
+
+// Handle key press of user
+function handleKeyPress(value) {
+    console.log(value, typeof(value));
+    let calcDisplayContent = value;
+    displayText.textContent = calcDisplayContent;
+}
